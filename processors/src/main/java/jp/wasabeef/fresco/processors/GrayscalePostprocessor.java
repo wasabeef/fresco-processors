074e063174d9dc903af5a6842191c399cb7c4858
wasabeef <dadadada.chop@gmail.com>
2020-09-27 03:48:08 +0900

    update: copyright

--- processors/src/main/java/jp/wasabeef/fresco/processors/GrayscalePostprocessor.java
+++ processors/src/main/java/jp/wasabeef/fresco/processors/GrayscalePostprocessor.java
@@ -1,8 +1,8 @@
 package jp.wasabeef.fresco.processors;
 
 /**
- * Copyright (C) 2017 Wasabeef
- *
+ * Copyright (C) 2020 Wasabeef
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
